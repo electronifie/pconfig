e14c3d84852beb21606ab0ad21a2e30c11584574
Anthony Schneider <anthony.schneider@electronifie.com>
2016-01-29 14:15:13 -0500

    Align with cconfig behavior.

--- pconfig.py
+++ pconfig.py
@@ -1,7 +1,21 @@
 import os, json
 
-def load(path):
+def load(path="config.json"):
+    config_envs = os.environ.get("CONFIG_ENVIRONMENTS")
+    
+    if config_envs is None:
+        config_envs = [ "development", "qa", "production" ]
+    else:
+        config_envs = str.split(config_envs, ",")
+
     env = os.environ.get("PYTHON_ENV") or "development"
-    return json.loads(open(path, "r").read()).get(env)
+    config = json.loads(open(path, "r").read())
+    config[env] = config.get(env) or {}
+
+    for k in config.keys():
+        if k not in config_envs:
+            config[env][k] = config[k]
+    
+    return config[env]
 
         
